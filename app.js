3fdf05dc7373965665739b4839c144955c2aaeb2
ammarbakheit <amarbakheit99@gmail.com>
2022-03-21 21:51:04 +0200

    feat: add routing

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const  express  = require("express");
 const config = require("./config/config.js");
+const router = require("./routes");
 
 const app = express();
 
 const port = config.port;
 const host = config.host;
 
+app.use("/", router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.listen("1200", "localhost", () => {
     console.log(`Server Listening at http://${host}:${port}`);
-  
-})  
+
+   
+});   
